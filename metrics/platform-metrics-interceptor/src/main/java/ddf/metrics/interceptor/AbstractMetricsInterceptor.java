aeb4cade3e9b1ef85916afade99330e14836a432
tbatieConnexta <tracy.batie@connexta.com>
2015-06-17 09:20:54 -0700

    DDF-1247: Catalog Queries Total Results count continues to be reported until new Query is executed   - Fixed variable spelling

--- metrics/platform-metrics-interceptor/src/main/java/ddf/metrics/interceptor/AbstractMetricsInterceptor.java
+++ metrics/platform-metrics-interceptor/src/main/java/ddf/metrics/interceptor/AbstractMetricsInterceptor.java
@@ -14,6 +14,8 @@
  **/
 package ddf.metrics.interceptor;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.cxf.message.Exchange;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
@@ -23,12 +25,13 @@ import org.slf4j.LoggerFactory;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SlidingTimeWindowReservoir;
 
 /**
  * This class is extended by the metrics interceptors used for capturing round trip message latency.
- * 
+ *
  * @author willisod
- * 
+ *
  */
 public abstract class AbstractMetricsInterceptor extends AbstractPhaseInterceptor<Message> {
 
@@ -38,24 +41,22 @@ public abstract class AbstractMetricsInterceptor extends AbstractPhaseIntercepto
 
     private static final String HISTOGRAM_NAME = "Latency";
 
-    private static final MetricRegistry metrics = new MetricRegistry();
+    private static final MetricRegistry METRICS = new MetricRegistry();
 
-    private static final JmxReporter reporter = JmxReporter.forRegistry(metrics)
+    private static final JmxReporter reporter = JmxReporter.forRegistry(METRICS)
             .inDomain(REGISTRY_NAME).build();
 
-    final Histogram messageLatency;
+    static final Histogram MESSAGE_LATENCY = METRICS.register(MetricRegistry.name(HISTOGRAM_NAME), new Histogram(new SlidingTimeWindowReservoir(1, TimeUnit.MINUTES)));
 
     /**
      * Constructor to pass the phase to {@code AbstractPhaseInterceptor} and creates a new
      * histogram.
-     * 
+     *
      * @param phase
      */
     public AbstractMetricsInterceptor(String phase) {
 
         super(phase);
-
-        messageLatency = metrics.histogram(MetricRegistry.name(HISTOGRAM_NAME));
 
         reporter.start();
     }
@@ -98,7 +99,7 @@ public abstract class AbstractMetricsInterceptor extends AbstractPhaseIntercepto
     }
 
     private void increaseCounter(Exchange ex, LatencyTimeRecorder ltr) {
-        messageLatency.update(ltr.getLatencyTime());
+        MESSAGE_LATENCY.update(ltr.getLatencyTime());
     }
 
 }
