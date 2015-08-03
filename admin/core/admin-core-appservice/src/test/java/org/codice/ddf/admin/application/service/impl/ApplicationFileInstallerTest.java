0efbbaa8c2d7a245e7adb1378d5ac0bffe7078c9
NGoss <nathan.goss@wyle.com>
2015-08-03 10:09:27 -0400

    DDF-1172 Code coverage for error states and logger
    Added checks for logger output to improve code coverage in
    ApplicationFileInstaller, and also added several new checks for fail
    states and exception blocks. In addition, added a setUp method which
    initializes the logger checks in order to reduce redundancy.

--- admin/core/admin-core-appservice/src/test/java/org/codice/ddf/admin/application/service/impl/ApplicationFileInstallerTest.java
+++ admin/core/admin-core-appservice/src/test/java/org/codice/ddf/admin/application/service/impl/ApplicationFileInstallerTest.java
@@ -13,53 +13,187 @@
  */
 package org.codice.ddf.admin.application.service.impl;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 
+import org.codice.ddf.admin.application.service.ApplicationServiceException;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
+
 public class ApplicationFileInstallerTest {
+
+    private static final String TEST_FILE_NAME = "/test-kar.zip";
+
+    private static final String BAD_FILE_NAME = "/test-badfile";
+
+    private static final String BAD_ZIP_NAME = "/test-badzip.zip";
+
+    private static final String MAIN_FEATURE_NAME = "main-feature";
+
+    private static final String MAIN_FEATURE_VERSION = "1.0.1";
+
+    private static final String INVALID_ZIP_STRING = "Got an error when trying to read the application as a zip file.";
+
+    private static final String IOEX_STRING = "Could not write out file.";
+
+    private static final String INSTALL_IOEX = "Got an error when trying to read the incoming application.";
 
     private Logger logger = LoggerFactory.getLogger(ApplicationFileInstaller.class);
 
+    private Appender mockAppender;
+
+    private ch.qos.logback.classic.Logger root;
+
+    @Before
+    public void setUpLogger() {
+        root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        mockAppender = mock(Appender.class);
+        when(mockAppender.getName()).thenReturn("MOCK");
+        root.addAppender(mockAppender);
+        root.setLevel(Level.ALL);
+    }
+
     /**
      * Tests the {@link ApplicationFileInstaller#install(File)} method
+     *
+     * @Throws Exception
      */
     @Test
-    public void testInstall() {
-        ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
-        File testFile = new File(File.class.getResource("/test-kar.zip").getPath());
+    public void testInstall() throws Exception {
 
-        try {
-            assertNotNull(testInstaller.install(testFile));
-        } catch (Exception e) {
-            logger.info("Exception: ", e);
-            fail();
-        }
+        ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
+        File testFile = new File(File.class.getResource(TEST_FILE_NAME).getPath());
+
+        assertTrue("Returned URI should have a unix-style path",
+                testInstaller.install(testFile).getPath().matches("(/.+)+"));
+
+        verify(mockAppender).doAppend(argThat(new ArgumentMatcher() {
+            @Override
+            public boolean matches(final Object argument) {
+                return ((LoggingEvent) argument).getFormattedMessage().contains(IOEX_STRING);
+            }
+        }));
+    }
+
+    /**
+     * Tests the {@link ApplicationFileInstaller#install(File)} method
+     * for the case where the file given is not a valid zip file
+     *
+     * @throws Exception
+     */
+    @Test(expected = ApplicationServiceException.class)
+    public void testInstallInvalidZip() throws Exception {
+        ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
+        File testFile = new File(File.class.getResource(BAD_FILE_NAME).getPath());
+
+        testInstaller.install(testFile);
+
+        verify(mockAppender).doAppend(argThat(new ArgumentMatcher() {
+            @Override
+            public boolean matches(final Object argument) {
+                return ((LoggingEvent) argument).getFormattedMessage().contains(INVALID_ZIP_STRING);
+            }
+        }));
+    }
+
+    /**
+     * Tests the {@link ApplicationFileInstaller#install(File)} method
+     * for the case where the file contains no features
+     *
+     * @Throws Exception
+     */
+    @Test(expected = ApplicationServiceException.class)
+    public void testInstallInvalidFile() throws Exception {
+
+        ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
+        File testFile = new File(File.class.getResource(BAD_ZIP_NAME).getPath());
+
+        testInstaller.install(testFile);
+    }
+
+    /**
+     * Tests the {@link ApplicationFileInstaller#install(File)} method
+     * for the case where an IOException is thrown when the ZipFile is created
+     *
+     * @throws Exception
+     */
+    @Test(expected = ApplicationServiceException.class)
+    public void testInstallZipFileIOEx() throws Exception {
+        ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
+        File testFile = mock(File.class);
+        when(testFile.getPath()).thenReturn("TestPath");
+        when(testFile.lastModified()).thenReturn((long) 1);
+
+        testInstaller.install(testFile);
+
+        verify(mockAppender).doAppend(argThat(new ArgumentMatcher() {
+            @Override
+            public boolean matches(final Object argument) {
+                return ((LoggingEvent) argument).getFormattedMessage().contains(INSTALL_IOEX);
+            }
+        }));
     }
 
     /**
      * Tests the {@link ApplicationFileInstaller#getAppDetails(File)} method
+     *
+     * @Throws Exception
      */
     @Test
-    public void testGetAppDetails() {
+    public void testGetAppDetails() throws Exception {
         ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
-        File testFile = new File(File.class.getResource("/test-kar.zip").getPath());
+        File testFile = new File(File.class.getResource(TEST_FILE_NAME).getPath());
         ZipFileApplicationDetails testFileDetails;
-        try {
-            testFileDetails = testInstaller.getAppDetails(testFile);
-            assertNotNull(testFileDetails);
-            //Verify other stuff
-            assertEquals("main-feature", testFileDetails.getName());
-            assertEquals("1.0.1", testFileDetails.getVersion());
-        } catch (Exception e) {
-            logger.info("Exception: ", e);
-            fail();
-        }
+
+        testFileDetails = testInstaller.getAppDetails(testFile);
+
+        assertNotNull(testFileDetails);
+        assertEquals(MAIN_FEATURE_NAME, testFileDetails.getName());
+        assertEquals(MAIN_FEATURE_VERSION, testFileDetails.getVersion());
+    }
+
+    /**
+     * Tests the {@link ApplicationFileInstaller#getAppDetails(File)} method
+     * for the case where a ZipException is thrown
+     *
+     * @Throws Exception
+     */
+    @Test(expected = ApplicationServiceException.class)
+    public void testGetAppDetailsZipException() throws Exception {
+        ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
+        File testFile = new File(File.class.getResource(BAD_FILE_NAME).getPath());
+
+        testInstaller.getAppDetails(testFile);
+    }
+
+    /**
+     * Tests the {@link ApplicationFileInstaller#getAppDetails(File)} method
+     * for the case where an IOException is thrown when the ZipFile is created
+     *
+     * @throws Exception
+     */
+    @Test(expected = ApplicationServiceException.class)
+    public void testGetAppDetailsIOException() throws Exception {
+        ApplicationFileInstaller testInstaller = new ApplicationFileInstaller();
+        File testFile = mock(File.class);
+        when(testFile.getPath()).thenReturn("TestPath");
+        when(testFile.lastModified()).thenReturn((long) 1);
+
+        testInstaller.getAppDetails(testFile);
     }
 }
